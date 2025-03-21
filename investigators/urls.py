ae69318a4b3b1f593f55feee2fec24bfc7c480a1
amirmx2905 <amir.flores.cardona@gmail.com>
2025-03-20 19:11:01 -0600

    ADDED logout, not verified yet. Also ADDED requirements.txt

--- investigators/urls.py
+++ investigators/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_view, home_view
+from .views import login_view, home_view, logout_view
 
 urlpatterns = [
     path('', login_view, name='index'), 
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'), 
+    path('logout/', logout_view, name='logout'),
 ]
