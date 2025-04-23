3cc7bff5116fafb6700bec94d12654b67d8b4495
alan omar hernandez bella <watchfulink@MacBook-Air-de-alan.local>
2025-04-22 19:57:32 -0600

    jefe_area serializer listo con llaves foraneas

--- backend/investigators/serializers/jefe_area_serializer.py
+++ backend/investigators/serializers/jefe_area_serializer.py
@@ -2,6 +2,24 @@ from rest_framework import serializers
 from investigators.models import JefeArea
 
 class JefeAreaSerializer(serializers.ModelSerializer):
+    area_nombre = serializers.SerializerMethodField()
+    investigador_nombre = serializers.SerializerMethodField()
+    
     class Meta:
         model = JefeArea
-        fields = '__all__'
+        fields = [
+            'id',
+            'fecha_inicio',
+            'fecha_fin',
+            'activo',
+            'area',
+            'area_nombre',
+            'investigador',
+            'investigador_nombre'
+        ]
+        
+    def get_area_nombre(self, obj):
+        return obj.area.nombre if obj.area else None
+    
+    def get_investigador_nombre(self, obj):
+        return obj.investigador.nombre if obj.investigador else None
